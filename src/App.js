87fde8585e517fb8cf9fbb64c6044233b3c67be7
root <root@DESKTOP-HC7G8IA>
2025-04-10 05:38:57 +0300

    Last  commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,99 @@
+import { React, useState, useEffect } from 'react';
+import Form from './Components/Form';
+import TodoList from './Components/TodoList';
 import './App.css';
 import Timer from './Components/Timer';
 import Settings from './Components/Settings';
-import {useState} from 'react';
 import SettingsContext from './Components/SettingsContext';
 
+const App = () => {
+    
+    //To-do
+    const [inputText, setInputText] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [status, setStatus] = useState('all');
+    const [filteredTodos, setFilteredTodos] = useState([]);
+    
+    //Timer
+    const [showSettings, setShowSettings] = useState(false);
+    const [workMinutes, setWorkMinutes] = useState(25);
+    const[breakMinutes, setBrackeMinutes] = useState(5);
 
-function App() {
+    //UseEffect
+    useEffect(() => {
+        getLocalTodos();
+    }, []);
 
-  const [showSettings, setShowSettings] = useState(false);
-  const [workMinutes, setWorkMinutes] = useState(25);
-  const[breakMinutes, setBrackeMinutes] = useState(5);
+    //useEfect
+    useEffect(() => {
+        filterHandler();
+        if (todos.length > 0) {
+            saveLocalTodos();
+        }
+    }, [todos, status]);
 
-  return (
-    <main>
-      <SettingsContext.Provider value={{
-        showSettings,
-        setShowSettings,
-        workMinutes,
-        breakMinutes,
-        setWorkMinutes,
-        setBrackeMinutes,
-      }}>
-        {showSettings ? <Settings /> : <Timer /> }
-      </SettingsContext.Provider>
-    </main>
-  );
-}
+
+    //Filter Handler
+    const filterHandler = () => {
+        switch (status) {
+            case 'completed':
+                setFilteredTodos(todos.filter((todo) => todo.completed === true));
+                break;
+
+            case 'uncompleted':
+                setFilteredTodos(todos.filter((todo) => todo.completed === false));
+                break;
+
+            default:
+                setFilteredTodos(todos);
+                break;
+        }
+    };
+
+    //Save to local
+    const saveLocalTodos = () => {
+        console.log("Saving todos to localStorage:", todos);
+        localStorage.setItem("todos", JSON.stringify(todos));
+    };
+
+    const getLocalTodos = () => {
+        if (localStorage.getItem("todos") === null) {
+            console.log("No todos in localStorage, initializing with an empty array.");
+            localStorage.setItem("todos", JSON.stringify([]));
+        } else {
+            let todoLocal = JSON.parse(localStorage.getItem("todos"));
+            console.log("Parsed todos:", todoLocal);
+            setTodos(todoLocal);
+        }
+    };
+
+    
+
+
+    return (
+        <main>
+            <SettingsContext.Provider value={{
+                showSettings,
+                setShowSettings,
+                workMinutes,
+                breakMinutes,
+                setWorkMinutes,
+                setBrackeMinutes,
+            }}>
+                {showSettings ? <Settings /> : <Timer />}
+            </SettingsContext.Provider>
+            
+            <div className="App">
+                <h1 className="title">
+                    Todo list
+                </h1>
+                <Form setStatus={setStatus} todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
+                <TodoList filteredTodos={filteredTodos} setTodos={setTodos} todos={todos} />
+            </div>
+
+        </main>
+
+    );
+};
 
 export default App;
-
